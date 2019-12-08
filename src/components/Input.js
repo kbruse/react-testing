8423d4a7519b7c4bb6ce2d3388a8f37cc74664bf
kbruse <krzysztof.brusewicz@gmail.com>
2019-12-08 15:49:51 +0100

    feat(Input): add jsx to input component and render tests

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,12 +3,28 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
-    return (<div> <button></button> </div>)
+    const contents = this.props.succcess ? null : (
+      <form className="form-inline">
+        <input
+          type="text"
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          placeholder="enter guess"/>
+        <button
+          type="submit"
+          data-test="submit-button"
+          className="btn btn-primary">Submit</button>
+      </form>
+    );
+    return (
+      <div data-test="component-input">
+        { contents }
+      </div>)
   }
 }
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 };
 
 export default connect(mapStateToProps)(Input);
