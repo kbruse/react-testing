334ac4cc91b28d891c088c35d0b4d7e67fffce92
kbruse <krzysztof.brusewicz@gmail.com>
2019-12-08 16:47:52 +0100

    feat(middleware): add initial middleware to redux

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,10 +1,12 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
+import { middleware } from '../src/store/configureStore';
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 export const findByTestAttr = (wrapper, val) => {
